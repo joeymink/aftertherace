5041a294e50aae6a705fec07661f536a894bb87f
Joey Mink <jmink@optensity.com>
2014-11-26 21:29:56 -0500

    flesh out presentation of races in order of time

--- laps/views.py
+++ laps/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import get_object_or_404, render
 
 def races(request):
 	races = Race.objects.all().order_by('date')
+	years = []
+	dates = []
 	race_by_date={}
 	for race in races:
 		year = race.date.year
-		if not(year in race_by_date):
-			race_by_date[year] = {}
-		if not(race.date in race_by_date[year]):
-			race_by_date[year][race.date] = []
-		race_by_date[year][race.date].append(race)
-	print race_by_date
-	return render(request, 'laps/races.html', {'races_by_date': race_by_date })
+		if not(year in years):
+			years.append(year)
+		if not(race.date in dates):
+			dates.append(race.date)
+	return render(request, 'laps/races.html', {
+		'races':races,
+		'years':years,
+		'dates':dates })
 
 def race(request, race_id):
 	race = get_object_or_404(Race, pk=race_id)
