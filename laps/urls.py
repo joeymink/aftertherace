812dbabb0417bf5635584cc264f33082d803db98
Joey Mink <jmink@optensity.com>
2014-12-06 22:12:26 -0500

    add lap plot to race view

--- laps/urls.py
+++ laps/urls.py
@@ -4,7 +4,8 @@ from laps import views
 
 urlpatterns = patterns('',
     url(r'^races/$', views.races, name='races'),
-    url(r'^races/(?P<race_id>\d+)/', views.race, name='race'),
+    url(r'^races/(?P<race_id>\d+)/?$', views.race, name='race'),
+    url(r'^races/(?P<race_id>\d+)/chart_laps/?$', views.LapsAJAXView.as_view()),
     url(r'^machines/$', views.machines, name='machines'),
     url(r'^machines/(?P<machine_id>\d+)/', views.machine, name='machine'),
     url(r'^tracks/$', views.tracks, name='tracks'),
