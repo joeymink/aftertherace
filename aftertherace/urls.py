04f9c36fade307ac54883adaf71228f940d6fd4c
Joey Mink <jmink@optensity.com>
2014-12-10 21:01:51 -0500

    add necessary config to log users in

--- aftertherace/urls.py
+++ aftertherace/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import laps
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
 	url(r'^laps/', include('laps.urls', namespace='laps')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/?$', laps.views.index)
+    url(r'^/?$', laps.views.index),
+    url(r'^accounts/login/$', auth_views.login),
+
 )
