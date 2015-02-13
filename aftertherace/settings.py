f281aebe1ddb8deeef6544038997af70441f33f8
Joey Mink <joeymink@gmail.com>
2015-02-13 11:37:16 -0500

    fix syntax err in settings.py

--- aftertherace/settings.py
+++ aftertherace/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'laps'
     'laps',
     'django.contrib.sites',
     'registration',
