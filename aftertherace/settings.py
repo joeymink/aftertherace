c5fb8d8b49666ccce2f4568fdbe49ada805b9389
Joey Mink <joeymink@gmail.com>
2015-02-13 11:47:39 -0500

    fix botched merge of settings.py

--- aftertherace/settings.py
+++ aftertherace/settings.py
@@ -87,6 +87,36 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/racers/profile'
 
+# Heroku config:
+# (https://devcenter.heroku.com/articles/getting-started-with-django)
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# sendgrid config:
+DEFAULT_FROM_EMAIL = 'support@atr.tools'
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
 # registration
 ACCOUNT_ACTIVATION_DAYS = 30
 SITE_ID = 1
