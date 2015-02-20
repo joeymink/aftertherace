f93e19f17a74858d41f39711b8d1994be37759cf
Joey Mink <joeymink@gmail.com>
2015-02-19 21:56:08 -0500

    fix tests which have been broken for too long

--- laps/tests.py
+++ laps/tests.py
@@ -2,11 +2,14 @@ from django.test import TestCase
 from laps.models import Machine, MachineConfiguration, ConfigurationAttribute
 from laps import models
 from datetime import datetime
+from django.contrib.auth import get_user_model
+from decimal import Decimal
 
 class MachineTest(TestCase):
 
 	def test_save(self):
-		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009)
+		u,created = get_user_model().objects.get_or_create(username='user1', email='user1@joeymink.com')
+		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009, user=u)
 		m.save()
 
 		mFromDb = Machine.objects.get(id=m.id)
@@ -15,7 +18,8 @@ class MachineTest(TestCase):
 class MachineConfigurationTest(TestCase):
 
 	def test_save(self):
-		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009)
+		u,created = get_user_model().objects.get_or_create(username='user1', email='user1@joeymink.com')
+		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009, user=u)
 		m.save()
 		c = MachineConfiguration(name='My Config', machine=m)
 		c.save()
@@ -30,15 +34,17 @@ class MachineConfigurationTest(TestCase):
 class ConfigurationAttributeTest(TestCase):
 
 	def test_save(self):
-		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009)
+		u,created = get_user_model().objects.get_or_create(username='user1', email='user1@joeymink.com')
+		m = Machine(name='Ninjette Test', make='Kawi', model='EX250', year=2009, user=u)
 		m.save()
 		c = MachineConfiguration(name='My Config', machine=m)
 		c.save()
-		attr = ConfigurationAttribute(key='chain', value='non-oring 520', machine_config=c)
+		attr = ConfigurationAttribute(key='chain', value='non-oring 520')
 		attr.save()
+		attr.machine_configurations.add(c)
 
 		readConfigAttr = ConfigurationAttribute.objects.get(id=attr.id)
-		self.assertTrue(readConfigAttr.machine_config.machine.model == 'EX250')
+		self.assertTrue(readConfigAttr.machine_configurations.get().name == c.name)
 
 		readMachine = Machine.objects.get(id=m.id)
 		self.assertTrue(readMachine.configurations.all()[0].attributes.all()[0].value == 'non-oring 520')
@@ -46,23 +52,25 @@ class ConfigurationAttributeTest(TestCase):
 class RaceTest(TestCase):
 
 	def test_five_lap_race(self):
-		m = Machine(name='Ninja 250', make='Kawasaki', model='Ninja 250', year=2009)
+		u,created = get_user_model().objects.get_or_create(username='user1', email='user1@joeymink.com')
+		m = Machine(name='Ninja 250', make='Kawasaki', model='Ninja 250', year=2009, user=u)
 		m.save()
 		c = MachineConfiguration(name='My Config', machine=m)
 		c.save()
-		attr = ConfigurationAttribute(key='chain', value='non-oring 520', machine_config=c)
+		attr = ConfigurationAttribute(key='chain', value='non-oring 520')
 		attr.save()
+		attr.machine_configurations.add(c)
 
 		t = models.Track(name="NJMP Thunderbolt")
 		t.save()
-		race = models.Race(name="Ultra Lightweight Thunderbike", track=t, machine_config=c, date=datetime.now())
+		race = models.Race(name="Ultra Lightweight Thunderbike", track=t, machine_config=c, date_time=datetime.now(), user=u)
 		race.save()
 
 		racer = models.Racer(first="Joey")
 		racer.save()
 
 		for i in range(0, 5):
-			l = models.Lap(race=race, num=(i + 1), time="1.21.091", racer=racer)
+			l = models.Lap(race=race, num=(i + 1), time=Decimal(1.21))
 			l.save()
 
 		self.assertTrue(models.Race.objects.get(id=race.id).laps.count() == 5)
