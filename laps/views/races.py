4280cedfed28e8316ef735436829ad3b0b43bca6
Joey Mink <joeymink@gmail.com>
2015-04-08 21:56:26 -0400

    fix bugs resulting from null machines and tracks (which happen after motolaptimes import)

--- laps/views/races.py
+++ laps/views/races.py
@@ -108,7 +108,7 @@ def edit_race(request, username, race_id):
 				race.track = Track.objects.get(name=form.cleaned_data['track_name'])
 				race.num_laps = form.cleaned_data['num_laps']
 				race.organization = form.cleaned_data['organization']
-				if not(race.machine_config.machine.name == form.cleaned_data['machine_name']):
+				if (race.machine_config is None) or not(race.machine_config.machine.name == form.cleaned_data['machine_name']):
 					# The machine was changed
 					machine = Machine.objects.get(name=form.cleaned_data['machine_name'], user=user)
 					race.machine_config = machine.empty_configuration()
